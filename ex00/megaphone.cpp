ba7f904d7912d0316b6e029bc00ad91388358eb2
Navid Rigi Ebrahimi <navid.rigi23@gmail.com>
2024-02-01 10:24:25 +0000

    created this directories: ex0{0..2} created makefile and megaphone.cpp for ex00, and it works correctly

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    else
+    {
+        for (int i = 1; i < argc; i++)
+        {
+            std::string arg = argv[i];
+            for (long unsigned j = 0; j < arg.size(); j++)
+                std::cout << (char)toupper(arg[j]);
+        }
+    }
+    std::cout << std::endl;
+}
