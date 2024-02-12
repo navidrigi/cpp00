c37132a373a4173734129a2695bc7d31f6002461
Navid Rigi Ebrahimi <navid.rigi23@gmail.com>
2024-02-12 20:06:08 +0000

    some comments

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -9,9 +9,12 @@ int main()
 	while (true)
 	{
 		std::cout << "\033[35mEnter the Command: \033[0m";
+		// cin >> for single characters
+		// getline -> line inputs
 		std::getline(std::cin, str);
 		if (str.empty())
 			continue;
+		// if (std::cin.eof() || std::cin.fail())
 		if (std::cin.eof())
 		{
 			std::cout << "EOF" << std::endl;
